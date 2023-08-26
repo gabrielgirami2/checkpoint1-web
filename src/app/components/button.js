889799f6edac0a0c385f69c2b187ad86ddaab6c7
Rafael.Chaves <rafaelchaveslv@gmail.com>
2023-08-26 14:50:24 -0300

    Co-authored-by: Gabriel Girami <gabrielgirami2@users.noreply.github.com>

--- src/app/components/button.js
+++ src/app/components/button.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
-const Button = styled.button;
+const Button = styled.button`
+    border: none; 
+    width: auto;
+    height: auto;
+    padding: 10px;
+`;
 
 export default Button;
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
-const Navbar = styled.nav;
+const Navbar = styled.nav`
+    width: 100%;
+    height: 50px;
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #00A1FC;
+    box-shadow: 0px 8px 23px -7px rgba(0, 0, 0, 0.5);
+`;
 
 export default Navbar;
